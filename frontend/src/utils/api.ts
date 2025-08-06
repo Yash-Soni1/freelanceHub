e6a56d14598d1cfe49b520bc6bd878f29eabcdb1
Yash Soni <yash2165soni@gmail.com>
2025-08-06 15:53:11 +0530

    Update api.ts

--- frontend/src/utils/api.ts
+++ frontend/src/utils/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://freelancehub-backend-h85j.onrender.com/api",
+  baseURL: "https://freelancehub-backend-h85j.onrender.com",
   withCredentials: false, 
   headers: {
     "Content-Type": "application/json",
